105c52e5351f3fb3c771bd5766999e0e3f21dde6
Maham-Javed <mahamjavedb35@gmail.com>
2024-12-06 22:28:29 +0500

    made a password generator project in react, by using useEffect, useCallback and useRef.

--- 03password-generator/src/App.jsx
+++ 03password-generator/src/App.jsx
@@ -1,10 +1,105 @@
-import { useState } from "react";
-import "./App.css";
+import { useState, useCallback, useEffect, useRef } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [password, setPassword] = useState("");
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
 
-  return <></>;
+  const passwordRef = useRef(null);
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTWXYZqwertyuiopasdfghjklzxcvbnm";
+
+    if (numberAllowed) {
+      str += "0987654321";
+    }
+    if (charAllowed) {
+      str += "`~!@#$%^&*()_+=-{}[]()<>,./';:,.";
+    }
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+
+    setPassword(pass);
+  }, [length, charAllowed, numberAllowed, setPassword]);
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numberAllowed, charAllowed, passwordGenerator]);
+
+  const copyToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
+
+  return (
+    <>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 pt-4 pb-4 bg-slate-700  text-orange-500">
+        <h1 className="text-center my-3">Password Generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={password}
+            placeholder="Password"
+            className="outline-none w-full py-1 px-3 mb-3 rounded-lg"
+            readOnly
+            ref={passwordRef}
+          />
+          <button
+            onClick={copyToClipboard}
+            className="outline-none bg-blue-700 text-white px-3 mb-3 rounded-lg"
+          >
+            Copy
+          </button>
+        </div>
+        {/* For chooses; length, character, number */}
+        <div className="flex text-sm gap-x-3">
+          {/* Range div */}
+          <div className="flex items-center gap-x-1">
+            <input
+              type="range"
+              value={length}
+              min={6}
+              max={100}
+              className="curser-pointer"
+              onChange={(e) => {
+                setLength(e.target.value);
+              }}
+            />
+            <label>Length {length}</label>
+          </div>
+          {/* Number div */}
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              id="numberInput"
+              onChange={() => {
+                setNumberAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="numberInput">Numbers</label>
+          </div>
+          {/* Character div */}
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id="charInput"
+              onChange={() => {
+                setCharAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="charInput">Character</label>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
